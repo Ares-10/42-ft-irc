d2f83209891635ccf1d562b4b72b59027e1ad5f2
Ares-10 <cholionking1@naver.com>
2025-03-06 21:00:56 +0900

    feat: Pass 명령 구현

--- srcs/command/Pass.cpp
+++ srcs/command/Pass.cpp
@@ -1,22 +1,14 @@
+#include <Error.hpp>
+
 #include "../../includes/Command.hpp"
 
 void Pass::execute()
 {
 	if (_args.size() < 1)
-	{
-		_client->write("461" + _client->getNickname() + "PASS :Not enough parameters");
-		return;
-	}
+		throw std::runtime_error(Error::err_needmoreparams(_client->getNickname(), "PASS"));
 	if (_client->getClientState() == REGISTERED)
-	{
-		_client->write("462" + _client->getNickname() + " :Unauthorized command (already registered)");
-		return;
-	}
+		throw std::runtime_error(Error::err_unauthorizedcommand(_client->getNickname(), "PASS"));
 	if (_args[0] != _server->getPassword())
-	{
-		_client->write("997" + _client->getNickname() + " :Incorrect password");
-		return;
-	}
-	else
-		_client->setClientState(REGISTERED);
+		throw std::runtime_error(Error::err_incorrectpassword(_client->getNickname(), "PASS"));
+	_client->setClientState(LOGIN);
 }
